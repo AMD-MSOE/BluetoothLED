36746e058593ffa604e6ee313e82561aeee6e39d
Tom Duex <hippyjonyo@gmail.com>
2013-03-18 15:55:09 -0500

    stuff from 3/18

--- src/com/amd/msoe/bluetoothled/MainActivity.java
+++ src/com/amd/msoe/bluetoothled/MainActivity.java
@@ -1,14 +1,22 @@
 package com.amd.msoe.bluetoothled;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,6 +27,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
+	private static final String TAG = "Bluetooth LED";
+	
 	private static final int REQUEST_ENABLE_BT = 5;
 	private BluetoothAdapter mBluetoothAdapter;
 	private ListView lstBluetoothDevices;
@@ -26,11 +36,29 @@ public class MainActivity extends Activity {
 	private List<String> bluetoothDevices;
 	private boolean ledOn;
 	private Set<BluetoothDevice> pairedDevices;
+	private IntentFilter intentFilter;
+	
+	private BroadcastReceiver intentReceiver = new BroadcastReceiver() {  // Abstract class
+        public void onReceive(Context context, Intent receivedIntent) {
+            if (BluetoothDevice.ACTION_FOUND.equals(receivedIntent.getAction())) 
+            {
+                BluetoothDevice foundDevice = receivedIntent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                
+                mArrayAdapter.add(foundDevice.getName() + "\n" + foundDevice.getAddress());
+		        mArrayAdapter.notifyDataSetChanged();
+		        
+                Log.v(TAG, foundDevice.getName()+" "+foundDevice.getAddress()+" was found");
+            }
+        }//END onReceive
+    };//END BroadcastReceiver
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		intentFilter = new IntentFilter();
+	    intentFilter.addAction("ACTION_FOUND");
 		
 		ledOn = false;
 		
@@ -59,6 +87,8 @@ public class MainActivity extends Activity {
 		if (!mBluetoothAdapter.isEnabled()) {
 		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		} else {
+			findBluetoothDevice();
 		}
 	}
 
@@ -84,6 +114,8 @@ public class MainActivity extends Activity {
 	
 	public void findBluetoothDevice() {
 		pairedDevices = mBluetoothAdapter.getBondedDevices();
+		mBluetoothAdapter.startDiscovery();
+	    
 		// If there are paired devices
 		if (pairedDevices.size() > 0) {
 		    // Loop through paired devices
@@ -96,11 +128,37 @@ public class MainActivity extends Activity {
 	}
 
 	public void sendLEDSignal(int position) {
-		if (ledOn) {
-			//send the off signal
-		} else {
-			//send the on signal
+		BluetoothDevice device = (BluetoothDevice)pairedDevices.toArray()[position];
+		try {
+			BluetoothSocket socket = device.createRfcommSocketToServiceRecord(UUID.fromString("23bfd07d-92a5-460d-a806-de1928cc7e85"));
+			Log.d(TAG,"waiting for connection");
+			socket.connect();
+			Log.d(TAG,"made connection");
+			if (ledOn) {
+				//send the off signal
+				socket.getOutputStream().write(0);
+			} else {
+				//send the on signal
+				socket.getOutputStream().write(255);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	protected void onResume() {
+	    super.onResume();
+	    registerReceiver(intentReceiver, intentFilter);
+	}
+
+	@Override
+	protected void onPause() {
+	    super.onPause();
+	    if (intentReceiver != null) {
+	        unregisterReceiver(intentReceiver);
+	    }
 	}
 }
 
